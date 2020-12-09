97176b8db977fa60a8f30977a81db73925ac9f66
Nicole Mastrostefano <nicole_mas@hotmail.it>
2020-12-09 16:20:04 +0100

    next/prev

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var app= new Vue(
+  {
+    el:"#root",
+    data: {
+      imageIndex:0,
+
+      images:[
+        "https://www.pronerding.it/wp-content/uploads/2019/08/lulu-copertina-.jpg",
+        "https://www.pronerding.it/wp-content/uploads/2019/08/janna-copertina-.jpg",
+        "https://www.pronerding.it/wp-content/uploads/2019/08/karma-copertina-.jpg",
+        "https://www.pronerding.it/wp-content/uploads/2019/08/syndra-copertina-.jpg"
+        ]
+
+    },
+    methods:{
+      nextImage: function(){
+        this.imageIndex ++;
+        if (this.imageIndex==this.images.length){
+          this.imageIndex=0
+        }
+      },
+      prevImage: function(){
+        this.imageIndex --;
+        if (this.imageIndex== -1){
+          this.imageIndex= this.images.length -1;
+        }
+
+      }
+    }
+  }
+);
